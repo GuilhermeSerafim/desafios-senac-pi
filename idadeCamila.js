99759fa78f9e7d7036758d9d4feb0df8d0859624
GuilhermeSerafim <guilerstudies@gmail.com>
2025-02-24 11:17:08 -0300

    Corrigindo idadeCamila

--- idadeCamila.js
+++ idadeCamila.js
@@ -6,10 +6,10 @@ const idade1 = prompt('Informe a primeira idade: ');
 const idade2 = prompt('Informe a segunda idade: ');
 const idade3 = prompt('Informe a terceira idade: ');
 
-if (idade1 > idade2 && idade1 < idade3) {
-    console.log(`Idade de Camila ${idade1}`);
-} else if (idade2 > idade1 && idade2 < idade3) {
-    console.log(`Idade de Camila ${idade2}`);
+if ((idade1 >= idade2 && idade1 <= idade3) || (idade1 <= idade2 && idade1 >= idade3)) {
+    console.log(`Idade de Camila: ${idade1}`);
+} else if ((idade2 >= idade1 && idade2 <= idade3) || (idade2 <= idade1 && idade2 >= idade3)) {
+    console.log(`Idade de Camila: ${idade2}`);
 } else {
-    console.log(`Idade de camila ${idade3}`);
-}
+    console.log(`Idade de Camila: ${idade3}`);
+} 
