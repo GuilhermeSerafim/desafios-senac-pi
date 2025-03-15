d5845a178a7297c77a5fc8c470beebd804e768e8
GuilhermeSerafim <guilerstudies@gmail.com>
2025-03-15 09:52:11 -0300

    Gerando boletim a partir de um objeto aluno, imprimindo o objeto aluno com o array de objetos Boletim - Desafio finalizado

--- aula2/Desafio.ts
+++ aula2/Desafio.ts
@@ -2,6 +2,11 @@ import promptSync from 'prompt-sync';
 
 const prompt = promptSync();
 
+type Aluno = {
+    nome: string,
+    disciplinas: Disciplina[]
+}
+
 type Disciplina = {
     nome: string,
     nota1: number,
@@ -9,11 +14,10 @@ type Disciplina = {
     nota3: number
 }
 
-type NotasAluno = {
-    nome: string,
-    disciplinas: Disciplina[]
-}
-
+// Situação:
+// Aprovado: média ≥ 7.0
+// Reprovado: média < 5.0
+// Recuperação: 5.0 ≤ média < 7.0 */
 type Boletim = {
     disciplina: string,
     media: number,
@@ -21,9 +25,9 @@ type Boletim = {
 }
 
 const nomeAluno = prompt("Informe o nome do aluno: ");
-
 const disciplinas: Disciplina[] = [];
 
+// Input disciplina e notas
 while (true) {
     const maisDisciplina = prompt("Quer informar uma disciplina? (sim/não): ").toLowerCase();
     if (maisDisciplina !== "sim") break;
@@ -39,11 +43,37 @@ while (true) {
     disciplinas.push({ nome: nomeDisciplina, nota1, nota2, nota3 });
 }
 
-const aluno: NotasAluno = {
+const alunoA: Aluno = {
     nome: nomeAluno,
     disciplinas: disciplinas,
 }
 
-function calcularBoletim(notas: NotasAluno): Boletim[] {
-    // TERMINAR
+
+// Percorre notas, calcular media e mostrar todos os boletins de cada respectiva materia do aluno
+function gerarBoletim(aluno: Aluno): Boletim[] {
+    const boletimArr: Boletim[] = [];
+    for (let i = 0; i < aluno.disciplinas.length; i++) {
+        let media = 0;
+        media = (aluno.disciplinas[i].nota1 + aluno.disciplinas[i].nota2 + aluno.disciplinas[i].nota3) / 3;
+        let boletim: Boletim = {
+            disciplina: aluno.disciplinas[i].nome,
+            media: media,
+            situacao: media > 6 ? "Aprovado" : media < 5 ? "Reprovado" : "Recuperação"
+        }
+        boletimArr.push(boletim);
+    }
+    return boletimArr;
 }
+
+function visualizarBoletimAluno(aluno: Aluno, boletins: Boletim[]): void { 
+    console.log(`Boletim do aluno ${aluno.nome}`);
+    for (const boletim of boletins) {
+        console.log("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+        console.log(boletim.disciplina);
+        console.log(boletim.media);
+        console.log(boletim.situacao);
+        console.log("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+    }
+}
+
+visualizarBoletimAluno(alunoA, gerarBoletim(alunoA));
